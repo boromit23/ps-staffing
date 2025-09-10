e2cd44512a388844b42efb62f98aaad6de8656ff
boromit23 <boromit.tv@gmail.com>
2025-09-10 09:02:24 -0400

    fix: use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header'
 import Hero from './components/Hero'
